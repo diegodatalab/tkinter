004878045be22517018e310bf656e84dd83a31b4
Diego <diegodatalab@gmail.com>
2023-09-05 04:14:45 -0300

    52. Intro

---
+++ 8_modules/52.py
@@ -0,0 +1,17 @@
+#
+# * 52. Intro
+
+import random
+import string
+import datetime
+
+# random_number = random.randint(0, 10)
+# print(random_number)
+
+test_list  = ['hello', False, 1,2,3,4,5]
+print(random.choice(test_list))
+
+print(string.ascii_lowercase)
+
+# print(dir(datetime))
+print(datetime.datetime(year=1984, month=10, day=25))
